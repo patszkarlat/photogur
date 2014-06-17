4a6154ae6a150eb789055cfb5cdb34ab98f503b4
patszkarlat <punkgeek@mac.com>
2014-06-17 14:20:50 -0400

    Refactoring Step #4 to use dynamic routes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,27 +1,44 @@
 class PicturesController < ApplicationController
-	# def index
-
-		def picture0
-		@picture = {
+	
+	def index
+		@pictures = [
+			{
 				:title => "The old church on the coast of White sea",
 				:artist => "Sergey Ershov",
 				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-			}
-	end
+			},
+			{
 
-	def picture1
-		@picture = {
 				:title => "Sea Power",
 				:artist => "Stephen Scullion",
 				:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-			}
-	end
-
-	def picture2
-		@picture = { 
+			},
+			{
+ 
 				:title => "Into the Poppies",
 				:artist => "John Wilhelm",
 				:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
 			}
-		end
+		]
 end
+
+	def show
+		@pictures = [
+			{
+				:title => "The old church on the coast of White sea",
+				:artist => "Sergey Ershov",
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+			},
+			{
+				:title => "Sea Power",
+				:artist => "Stephen Scullion",
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+			},
+			{
+				:title => "Into the Poppies",
+				:artist => "John Wilhelm",
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+			}
+		]
+		@picture = @pictures[params[:id].to_i]
+	end
