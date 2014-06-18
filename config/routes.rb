defdbf9c22c01f3d5559c78c603d5e66b0ed1471
patszkarlat <punkgeek@mac.com>
2014-06-18 11:38:16 -0400

    Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,8 @@
-Rails.application.routes.draw do
-
 Photogur::Application.routes.draw do 
   resources :pictures
 
-  root :to => "pictures#index"
+  root :to => "pictures#index" 
 
-  get 'pictures' => 'pictures#index'
-
-  get 'pictures' => 'pictures#index'
-
-  post 'pictures' => "pictures#create"
-
-  get 'pictures/new' => 'pictures#new'
-
-  get 'pictures/:id' => 'pictures#show'
-
-  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
-
-  patch 'pictures/:id' => "pictures#update"
-
-  delete 'pictures/:id' => 'pictures#destroy', as: "delete_picture"
-
-  root :to "pictures#index"
 end
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -78,4 +59,4 @@ end
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
